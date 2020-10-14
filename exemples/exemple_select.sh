5d0a102a7fcdb7b2204344012fa329a7f476ae91
Christophe Blaess <christophe.blaess@logilin.fr>
2020-10-14 13:57:19 +0200

    [FIX] `exemple_select.sh` after passing `shellcheck`

--- exemples/exemple_select.sh
+++ exemples/exemple_select.sh
@@ -1,63 +1,62 @@
 #! /bin/sh
 
-function action_sur_fichier
+action_sur_fichier()
 {
     local reponse
     local saisie
     echo "*********************************************"
-    # Préparer la chaîne d'invite PS3
+    # Preparer la chaine d'invite PS3
     PS3="Action sur $1 : "
 
-    # proposer un menu Informations/Copier/Déplacer/Détruire/Retour
-    select reponse in Informations Copier Déplacer Détruire Retour ; do
-	
-        # Agir en fonction de la réponse
-        case $reponse in
+    # proposer un menu
+    select reponse in Informations Copier Deplacer Detruire Retour ; do
+
+        # Agir en fonction de la repose
+        case "$reponse" in
             # Informations -> ls -l
-            Informations ) echo ; ls -l $1 ; echo ;;
+            Informations ) echo ; ls -l "$1" ; echo ;;
             # Copier -> demander nouveau nom puis cp
             Copier ) echo -n "Copier $1 vers ? "
                 if ! read saisie ; then continue ; fi
-                cp $1 $saisie ;;
-            # Déplacer -> nouvel emplacement, mv, et sortie
-            Déplacer ) echo -n "Nouvel emplacement pour $1 ? "
+                cp "$1" "$saisie" ;;
+            # Deplacer -> nouvel emplacement, mv, et sortie
+            Deplacer ) echo -n "Nouvel emplacement pour $1 ? "
                 if ! read saisie ; then continue ; fi
-                mv $1 $saisie
+                mv "$1" "$saisie"
                 break ;;
-            # Détruire -> rm -i puis sortie
-            Détruire ) if rm -i $1 ; then break; fi ;;
+            # Detruire -> rm -i puis sortie
+            Detruire ) if rm -i "$1" ; then break; fi ;;
             # Retour -> sortie
             Retour ) break ;;
             * ) if [ "$REPLY" = "0" ] ; then break ; fi
-                echo "$REPLY n'est pas une réponse valide"
+                echo "$REPLY n'est pas une reponse valide"
                 echo ;;
         esac
     done
 }
- 
- 
-function liste_fichiers
+
+
+liste_fichiers()
 {
     echo "*********************************************"
-    # Préparer la chaîne d'invite PS3
-    PS3="Fichier à traiter : "
-	
+    PS3="Fichier a traiter : "
+
     # Afficher une liste des fichiers et l'option Quitter
     select fichier in * Quitter ; do
-	
-       # Si demande de Quitter, retour en échec
-        if [ "$fichier" == "Quitter" ] ; then return 1 ; fi
-        # Sinon, action sur le fichier sélectionné
-        # et retour en réussite
-        if [ ! -z "$fichier" ] ; then
-            action_sur_fichier $fichier
+
+       # Si demande de Quitter, retour en echec
+        if [ "$fichier" = "Quitter" ] ; then return 1 ; fi
+        # Sinon, action sur le fichier selectionne
+        # et retour en reussite
+        if [ -n "$fichier" ] ; then
+            action_sur_fichier "$fichier"
             return 0
         fi
     done
 }
- 
- 
-while true 
+
+
+while true
 do
 	if ! liste_fichiers
 	then
